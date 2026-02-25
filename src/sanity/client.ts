407a72b433088f89f01a9244d60ed66a87d2f549
mpo.coder <mpokodner@gmail.com>
2026-02-25 04:46:17 -0600

    sanity connection fix

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { SanityImageSource } from "@sanity/image-url";
 import { projectId, dataset, apiVersion } from "./env";
 
 export const client = createClient({
